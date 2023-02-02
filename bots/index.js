be24bdafbc52540c0b9ca0989ab6b6ba8422182f
Taim Sido <sidotaim02@gmail.com>
2023-02-02 09:27:08 -0800

    deleted msg code

--- bots/index.js
+++ bots/index.js
@@ -5,10 +5,5 @@ it.on('ready', () => {
     console.log(`Logged in as ${it.user.tag}!`);
 });
 
-it.on('message', msg => {
-    if (msg.content === 'it') {
-        msg.reply(' https://discord.gg/6CnaASVAu3');
-    }
-});
 
 it.login('your-token-here');
